64a1c42d04fdf38f438327e19b9a4786963f402c
Gabriela Dodson <dodsongabi@gmail.com>
2024-09-08 12:59:25 -0400

    Create determinePerformanceRating function

--- cc_3.js
+++ cc_3.js
@@ -1,17 +1,30 @@
 //Create a Function to Calculate Average Sales
 
-function calculateAveragesales (salesArray){
+function calculateAverageSales (salesArray){
     let total = 0;
     for (let sale of salesArray) {
         total += sale;
     }
         return total / salesArray.length;
 }
-        let sales = [100,150,200,250,300];
+        let sales = [1000,1500,4000,6000,6500];
 console.log (`Average sales: $$
-    {calculateAveragesales(sales)}`);
+    {calculateAverageSales(sales)}`);
 
-    
 
+
+//Create a function for Performance Ratings
+function determinePerformanceRating (averageSales) {
+    if (averageSales>10000) {
+ return "Excellent";
+} else if (averageSales>=7000 && averageSales<=10000) {
+    return "Good";
+} else if (averageSales>= 4000 && averageSales < 7000){
+return "Satisfactory";
+} else{
+return "Needs Improvement";
+}
+
+//example console.log(determinePerformanceRating(1500))
 
 
