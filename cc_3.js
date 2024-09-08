786b19ea6210650fa3e4491b3585f0358218f031
Gabriela Dodson <dodsongabi@gmail.com>
2024-09-08 15:49:14 -0400

    Test functions with sample sales data

--- cc_3.js
+++ cc_3.js
@@ -74,16 +74,12 @@ return report;
 
 
 
+//Testing Functions with Sample Data
+const salesData = [
+    {name:'Alice', sales: [12000,15000,13000]},
+    {name:'Bob', sales: [7000,6000,7500]},
+    {name:'Charlie', sales: [3000,4000,3500]},
+    {name:'Diana', sales: [9000,8500,9200]}];
 
-
-
-
-
-
-
-
-
-
-
-
-
+    console.log(generatePerformanceReport(salesData));
+    
